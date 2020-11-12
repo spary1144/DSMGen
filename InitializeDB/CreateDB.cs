ae99d4a194899a09a374ed84c1f8117037174bbd
SrOParker <iamsroparker@gmail.com>
2020-11-12 09:25:10 +0100

    Prueba de cambio

--- InitializeDB/CreateDB.cs
+++ InitializeDB/CreateDB.cs
@@ -84,7 +84,10 @@ public static void InitializeData ()
                 // customer.New_ (p_user:"user", p_password:"1234");
                 UsuarioCEN usucen = new UsuarioCEN();
                 usucen.New_("Jorge", "1234");
-                //Laureano eres gilipollas
+
+
+                //cambio
+                
                 //Esto es un cambio
 
                 /*PROTECTED REGION END*/
